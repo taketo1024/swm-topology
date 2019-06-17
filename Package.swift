5def5ffb425981a4de42488bd18f3d18992e0f33
Taketo Sano <taketo1024@gmail.com>
2019-06-17 11:14:50 +0900

    update dependencies.

--- Package.swift
+++ Package.swift
@@ -6,15 +6,13 @@ import PackageDescription
 let package = Package(
     name: "SwiftyTopology",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "SwiftyTopology",
             targets: ["SwiftyTopology"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/taketo1024/SwiftyMath.git", from: "1.0.4"),
-        .package(url: "https://github.com/taketo1024/SwiftyMath-homology.git", from: "1.0.4"),
+        .package(url: "https://github.com/taketo1024/SwiftyMath.git", .exact("1.0.12")),
+        .package(url: "https://github.com/taketo1024/SwiftyMath-homology.git", .exact("1.0.7")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
